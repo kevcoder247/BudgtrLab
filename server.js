45dd881f2097e0c2783d230bf0fca08592067ef9
)- <a-@-s-MacBook-Pro.local>
2022-05-27 10:21:54 -0400

    new post feature added and redirects back to index

--- server.js
+++ server.js
@@ -3,33 +3,37 @@ const budget = require('./models/budget')
 const app = express();
 const port = 3000;
 
+//MIDDLEWARE================================
+// app.use((req, res, next) => {
+//     console.log('I run for all routes')
+//     next();
+// });
+app.use(express.urlencoded({ extended: false }))
+
 //Routes
 
-//Index=================
+//Index=====================================
 app.get('/budgets', (req, res) => {
     res.render('index.ejs', {budget: budget})
 })
 
-//New==================
+//New=======================================
 app.get('/budgets/new', (req, res) => {
     res.render('new.ejs')
 })
 
-//Create===============
-app.post('/butgets', (req, res) => {
-    res.send('Budgtr Post')
+//Create======================================
+app.post('/budgets', (req, res) => {
+    budget.push(req.body);
+    // console.log(budget);
+    res.redirect('/budgets')
 })
 
-app.post('/fruits', (req, res) => {
-    res.send('post page')
-})
-
-
-//Show==================
+//Show=========================================
 app.get('/budgets/:index', (req, res) => {
     res.render('show.ejs', {budget: budget[req.params.index]})
 })
-
+//Tell The APP WHERE TO LISTEN
 app.listen(port, () => {
     console.log(`Listening on `, port)
 })
